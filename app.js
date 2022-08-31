9d78d57777330ae6f1be2b35da9bf62ca30487ce
Ayushi Rai <ayushicodes@gmail.com>
2022-08-31 18:53:43 +0530

    mole added

---
+++ app.js
@@ -0,0 +1,16 @@
+let square = document.querySelectorAll('.square')
+let mole = document.querySelector('.mole')
+let timeLeft = document.querySelector('time-left')
+let score = document.querySelector('score')
+let result = 0
+
+function randomNumber() {
+
+    square.forEach(square => {
+        square.classList.remove('mole')
+    });
+    let randomPosition = square[Math.floor(Math.random() * 9)]
+    randomPosition.classList.add('mole')
+}
+
+randomNumber()
