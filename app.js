eb2c56a39a8ab61f430c7e250d2b2c88cec8e9e8
Ayushi Rai <ayushicodes@gmail.com>
2022-09-01 22:18:49 +0530

    result displayed

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
 let square = document.querySelectorAll('.square')
 let mole = document.querySelector('.mole')
-let timeLeft = document.querySelector('time-left')
-let score = document.querySelector('score')
+let timeLeft = document.querySelector('#time-left')
+let score = document.querySelector('#score')
 let result = 0
-
+let hitPosition;
 function randomNumber() {
 
     square.forEach(square => {
@@ -11,13 +11,35 @@ function randomNumber() {
     });
     let randomPosition = square[Math.floor(Math.random() * 9)]
     randomPosition.classList.add('mole')
+    hitPosition = randomPosition.id
 }
 
+square.forEach(function (square) {
+    square.addEventListener('mousedown', function () {
+        if (square.id == hitPosition) {
+            result++
+            console.log(result)
+            score.textContent = result
+            hitPosition = null
+        }
+    })
+
+})
+// square.forEach(square => {
+//     square.addEventListener('mousedown', () => {
+//         if (square.id == hitPosition) {
+//             result++
+//             score.textContent = result
+//             hitPosition = null
+//         }
+//     })
+// })
 
 
 function moveMole() {
     let timerId = null
-    timerId = setInterval(randomNumber, 500)
+    timerId = setInterval(randomNumber, 1000)
+
 }
 
 moveMole()
